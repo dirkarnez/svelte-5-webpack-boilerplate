6885a03f7924d83b79747cf1e470cb1c50bdc315
dirkarnez <smalldirkalex@gmail.com>
2025-02-16 05:55:01 +0800

    - update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,25 @@ export default {
 						},
 						emitCss: prod,
 						hotReload: !prod,
-						preprocess: sveltePreprocess({ sourceMap: !prod })
+						preprocess: sveltePreprocess({
+              sourceMap: !prod,
+              babel: {
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    {
+                      loose: true,
+                      // No need for babel to resolve modules
+                      modules: false,
+                      targets: {
+                        // ! Very important. Target es6+
+                        esmodules: true,
+                      },
+                    },
+                  ],
+                ],
+              }
+            })
 					}
 				}
 			},
